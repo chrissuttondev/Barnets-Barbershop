4d67cea7b118346b1323db5b6b1328bb36ed6f49
Chris Sutton <chrissutton1789@gmail.com>
2024-06-16 21:20:15 +0000

    Added placeholder hero image

--- barnets/urls.py
+++ barnets/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from home.views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
@@ -24,4 +26,4 @@ urlpatterns = [
     path('home/', home, name='home'),      # Home view also mapped to '/home/'
     path("appointments/", include("appointments.urls"), name="appointments-urls"), # noqa
     path('admin/', admin.site.urls),       # Django admin URLs
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
