39e22094aa37f50f4931d50aabec4514f825f247
Chris Sutton <chrissutton1789@gmail.com>
2024-06-29 20:13:31 +0000

    Add heroku to ALLOWED_HOSTS

--- barnets/settings.py
+++ barnets/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-chrissutton-barnetsbarb-r2gdu15hftr.ws-eu114.gitpod.io']
+ALLOWED_HOSTS = ['8000-chrissutton-barnetsbarb-r2gdu15hftr.ws-eu114.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
